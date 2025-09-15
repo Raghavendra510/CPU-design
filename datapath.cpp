4b2ee1ff9bdcc93a0b5f132f2adfa404c20bce7d
Raghavendra510 <157114154+Raghavendra510@users.noreply.github.com>
2025-09-15 02:01:02 -0400

    Create datapath.cpp

---
+++ datapath.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+int main(){
+    fstream f("output.txt",ios::in|ios::out);
+    vector<string>machinecode;
+    string line;
+    while(getline(f,line)){
+        machinecode.push_back(line);
+        
+    }
+    
+    int pc=0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
