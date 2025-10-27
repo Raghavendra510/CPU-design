849440bb264f14c79b1faab41199e62e6f26b919
Pidathala Reddy Raghavendra <157114154+Raghavendra510@users.noreply.github.com>
2025-10-27 11:29:20 +0530

    Update datapath.cpp

--- datapath.cpp
+++ datapath.cpp
@@ -3,12 +3,6 @@ using namespace std;
 
 
 
-
-
-
-
-
-
 class insMem{
     public:
   vector<string>instructions;
@@ -28,7 +22,7 @@ class insMem{
 
 class regFile{
      public:
-    vector<int>regs(32);
+    vector<int>regs(33);
     int rsl1;
     int rsl2;
     int rdl;
@@ -40,7 +34,7 @@ class regFile{
     
     
     
- void  reg_fill(string ins,bool regread,bool regwrite){
+ void  reg_fill1(string ins,bool regread,bool regwrite){
         string temp=ins.substr(7,5);
          rsl2=stoi(temp,nullptr,2);
         temp=ins.substr(19,5);
@@ -51,9 +45,15 @@ class regFile{
         rs1=regs[rsl1];
         rs2=regs[rsl2];
         }
+      
+    }
+ void  reg_fill2(string ins,bool regread,bool regwrite){
+        string temp=ins.substr(20,5);
+        rdl=stoi(temp,nullpt,2);
+      
         if(regwrite){
             regs[rdl]=w_data;
-        }
+         }
     }
     
 };
@@ -64,7 +64,7 @@ class immgen
   public :
   int imm;
   string ins;
-  immgen(string ins){
+ void ig(string ins){
       this->ins=ins;
       string opcode=ins.substr(25.7);
      if(opcode=="0110011"){
@@ -354,7 +354,7 @@ class controlPath{
   bool jump;
   
   
-  controlPath(string opcode){
+  void cw(string opcode){
       if(opcode=="0110011"){
               Alusrc=0;
              memtoreg=0;
@@ -445,7 +445,33 @@ class controlPath{
 };
 
 
-
+class mainmem{
+  vector<int>mem(1e9);
+  bool memwrite;
+  bool memread;
+  int EA;
+  int writedata;
+  int ldres;
+  
+  
+  void memunit(int ea,int wd,bool mr,bool mw){
+      memwrite=mw;
+      memread=mr;
+      writedata=wd;
+      EA=ea;
+      
+      if(mr==1){
+          ldres=mem[EA];
+          
+      }
+      if(mw==1){
+          mem[EA]=writedata;
+      }
+      
+      
+  }
+    
+};
 
 
 
@@ -469,6 +495,74 @@ class controlPath{
      }
     
     int pc=0;
+    insMem InsMem(machinecode);
+    regFile RegFile;
+    immgen ImmGen;
+    ALUcontrol ALUControl;
+    ALU alu;
+    controlPath ControlPath;
+    mainmem MainMem;
+    
+    //cpu 
+    while(pc<machinecode.size()){
+        
+        string cur_ins=InsMem.fetch(pc);
+       cw(cur_ins.substr(25,7));
+        RegFile.reg_fill1(cur_ins,ControlPath.regread,ControlPath.regwrite);
+        ImmGen.ig(cur_ins);
+        int  a=RegFile.rs1;
+        int b;
+        if(ControlPath.Alusrc==1){
+            b=ImmGen.imm
+        }
+        else {
+            b=RegFile.rs2;
+        }
+        ALUControl.alucontrolgen(cur_ins,ControlPath.Aluop);
+        alu.compute(a,b,ALUControl.aluselect);
+        MainMem.memunit(alu.alures,RegFile.rs2,ControlPath.memread,ControlPath.memwrite);
+        int npc=pc+1;
+        int jpc=pc+ImmGen.imm;
+        int bpc=pc+ImmGen.imm;
+        int tpc;
+       
+        int m2;
+        if(ControlPath.branch&&alu.zeroflag){
+            m2=bpc;
+        }
+        else {
+            m2=npc;
+        }
+        if(ControlPath.jump){
+            tpc=jpc;
+        }
+        else {
+            tpc=m2;
+        }
+        
+       int erenjeager; 
+        if(ControlPath.jump==1){
+            erenjeager=npc;
+        }
+        else if(ControlPath.memtoreg==1){
+            erenjeager=MainMem.ldres;
+        }
+        else if(ControlPath.memtoreg==0){
+            erenjeager=alu.alures;
+        }
+        RegFile.reg_fill2(cur_ins,ControlPath.regread,ControlPath.regwrite);
+        
+        pc=tpc;
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
     
     
     
